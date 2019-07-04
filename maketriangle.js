2631abc7f9df8180758688b32a31cd6b2fc692fb
ChaiUrs <mysorechaitra@gmail.com>
2019-07-04 04:39:03 +0000

    Built a Triangle

---
+++ maketriangle.js
@@ -0,0 +1,19 @@
+function makeLine(length) {
+  var line = "";
+  for (var j=1; j<= length; j++)
+  {
+    line += "* "
+  }
+ return line + "\n";
+}
+
+function buildTriangle(height)
+{
+  var triangle = ""; 
+  for (var k=1; k<=height; k++)
+  {
+    triangle += (makeLine(k));
+  }
+  return triangle;
+}
+console.log ( buildTriangle(10) ); 
