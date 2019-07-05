6173aba3395aa71f140bc5a8f142e6227f7b00fd
ChaiUrs <mysorechaitra@gmail.com>
2019-07-05 21:17:43 +0000

    Seasons and Vacation program

---
+++ seasonsandvacation.js
@@ -0,0 +1,34 @@
+/*
+const seasons = ["spring", "summer", "fall"];
+const length = seasons.length;
+seasons.push("winter");
+console.log(length);
+console.log(seasons.length);
+*/
+
+
+/*
+const vacationDays = 2 + 3;
+if (vacationDays <= 5)
+{
+  console.log("Not enough for a long vacation.");
+}
+else if (vacationDays === 5)
+{
+  console.log("We can take a whole week off, not bad.");
+}
+else
+{
+  console.log("We're gonna have a great time!");
+}
+*/
+
+
+let sum = 3 + 2;
+let doubleSum = function(a, b)
+{
+  let sum = a + b;
+  return sum * 2;
+}
+doubleSum(5, 4);
+console.log(sum);
